cfa37d80abd0976df4a419ca1f1f5e076cce2003
lzy <li2453636559@163.com>
2019-01-04 09:30:12 +0800

    yi

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// require(['./js/config.js'], function() {
+//     require(['index'], function(index) {
+//         var xhr = new XMLHttpRequest();
+//         xhr.open('get', '/list');
+//         xhr.onload = function(e) {
+//             console.log(JSON.parse(e.target.responseText))
+//         }
+//         xhr.send();
+//     })
+// })
